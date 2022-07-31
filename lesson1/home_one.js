03ed40bc8d86bf51f357e77c26b16ff9fc2cbe34
demianblood <jokervanya2@gmail.com>
2022-07-31 22:10:44 +0300

    Initial commit

---
+++ lesson1/home_one.js
@@ -0,0 +1,44 @@
+// let hello ='hello';
+// let owu ='owu';
+// let com ='com';
+// let ua ='ua';
+// let one =1;
+// let ten =10;
+// let minus=-999;
+// let oneTwoThree=123;
+// let pi=3.14;
+// let twoSeven=2.7;
+// let sixteen =16;
+// let tru =true;
+// let fal =false;
+// console.log(hello,owu,com, ua, one, ten, minus, oneTwoThree, pi, twoSeven, sixteen, tru, fal);
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(minus);
+// alert(oneTwoThree);
+// alert(pi);
+// alert(twoSeven);
+// alert(sixteen);
+// alert(tru);
+// alert(fal);
+//
+//
+// document.write( hello,"",owu,"",com,"", ua,"", one,"", ten,"", minus,"", oneTwoThree,"", pi,"", twoSeven,"", sixteen,"", tru,"", fal)
+//
+// let firstName = 'Ivan';
+// let middleName = 'Rusakovich';
+// let lastName ='Anatolievich';
+// let name = `${firstName} ${middleName} ${lastName}`;
+// console.log(name);
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a, typeof b, typeof c)
+let a = prompt('say name');
+let b = prompt('say middle Name');
+let c =prompt('say year');
+console.log(`name ${a} middle Name ${b} year ${c}`);
